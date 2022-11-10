b96065522a7f331d11a1be0f0d775f42e072d2b7
Bo Jung <boyune01@gmail.com>
2022-11-09 23:26:05 -0500

    my WIP code

--- code/clean_input_data.py
+++ code/clean_input_data.py
@@ -6,16 +6,21 @@ import pandas as pd
 
 
 
-def read_n_sort_wea(file_dir):
+def read_data(file_dir):
     """
-    This function reads in weather data (which contains zenith angle, alzimuth angle, cloud cover, cloud cover opaque, pressue) in .csv format
+    This function reads in .csv data and outputs a pandas df.
+    The date input must be named exatly as follows:
+    date: 'DATE (MM/DD/YYYY)'
+    time: 'MST'
     INPUT - .csv file
     OUTPUT - pandas df
     """
     df = pd.read_csv(file_dir, parse_dates=[['DATE (MM/DD/YYYY)', 'MST']])
     print(df)
-    
+    return df
 
+
+# def merge_df():
 
 
 def main():
@@ -24,12 +29,23 @@ def main():
     # every mins
     wea_dir = "/Volumes/GoogleDrive/My Drive/COURSES/22 AU/CSE_583/final_prj/data/raw/2020/2020_wea.csv"
     # every 30 mins (12:15, 12:45) --> round to nearest hour (12:00, 1:00)
-    # prciptble_water_dir = 
+    precip_water_dir = "../data/raw/2020_precipitable_water.csv"
     # every 10 mins (12:10, 12:20, 12:30)
-    # aod_dir = 
+    aod_dir = "/Volumes/GoogleDrive/My Drive/COURSES/22 AU/CSE_583/final_prj/data/raw/2020/2020_aod_ssa_asymmetry.csv"
 
 
-    read_n_sort_wea(wea_dir)    
+    # wea_df = read_data(wea_dir)
+    prcp_wtr_df = read_data(precip_water_dir)
+    # aod_df = read_data(aod_dir)
+
+    # Precip_wtr data has time series every 15mins and 45mins. 
+    # This doesn't align with other data time series (i.e. aod - given every 10, 20, 30 etc mins).
+    # So, we need to round the time to nearest hour.
+    print(prcp_wtr_df)
+    prcp_wtr_df['DATE (MM/DD/YYYY)_MST'] = prcp_wtr_df['DATE (MM/DD/YYYY)_MST'].dt.ceil(freq='30T')
+    print(prcp_wtr_df)
+    
+
 
 
 if __name__ == "__main__":

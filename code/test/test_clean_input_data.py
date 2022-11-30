c7f76caa1e6e82e4c5a1ce3d4835609bcb8f14c5
Bo Jung <boyune01@gmail.com>
2022-11-30 17:27:23 -0500

    Modified test file.

--- code/test/test_clean_input_data.py
+++ code/test/test_clean_input_data.py
@@ -8,17 +8,23 @@ from code import clean_input_data
 data_dir = "/Volumes/GoogleDrive/My Drive/COURSES/22 AU/CSE_583/final_prj/data/raw/"
 
 class UnitTests(unittest.TestCase):
-    def test_read_wea_datas(self):
+    def test_read_wea_datas_smoke(self):
+        """
+        Smoke test to check the function runs.
+        """
+        file_dir = data_dir
+        clean_input_data(file_dir, 'wea')
+        
+        return
+    
+    def test_read_wea_datas_edge1(self):
         """
         Edge test to test for .csv format
         """
-        with self.assertRaises(ValueError):
-            for file in files:  # Q: WE WANT TO CHECK 'FILES' WHICH IS A VALUE INSIDE 'READ_WEA_DATAS' FUNCTION. HOW DO WE ACCESS A VALUE INSIDE A FUNCTION?
-                # Split the extension from the path and normalise it to lowercase.
-                ext = os.path.splitext(file)[-1].lower()
-                if ext == ".csv":
-                    pass
+        with self.assertRaises(TypeError):
+            file_dir = "...."
+            clean_input_data(file_dir, 'wea')
         
         return
 
-
+    def 

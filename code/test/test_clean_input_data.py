358563caefc1c599b5d18508aa72976f940bfe26
Joslyn Chen <mchen49@uw.edu>
2022-12-01 16:10:53 -0800

    Write some test

--- code/test/test_clean_input_data.py
+++ code/test/test_clean_input_data.py
@@ -9,43 +9,39 @@ data_dir = "/Volumes/GoogleDrive/My Drive/COURSES/22 AU/CSE_583/final_prj/data/r
 
 
 class UnitTests(unittest.TestCase):
-<<<<<<< HEAD
-    def smoke_test_read_wea_datas(self):
-        """
-        smoke test to check for file_dir exists.
-        """
-        with self.assertRaises(FileNotFoundError):
-            
-        return
 
-    def edge_test_read_wea_datas(self):
-=======
     def test_read_wea_datas_smoke(self):
         """
         Smoke test to check the function runs.
         """
         file_dir = data_dir
         clean_input_data(file_dir, 'wea')
-        
         return
-    
-    def test_read_wea_datas_edge1(self):
->>>>>>> e6dc182c2c328896ca1966757eb86587862523ad
+
+    def test_read_wea_datas_edge(self):
         """
         Edge test to test for .csv format
         """
         with self.assertRaises(TypeError):
-<<<<<<< HEAD
             for file in files:  # Q: WE WANT TO CHECK 'FILES' WHICH IS A VALUE INSIDE 'READ_WEA_DATAS' FUNCTION. HOW DO WE ACCESS A VALUE INSIDE A FUNCTION?
                 # Split the extension from the path and normalise it to lowercase.
                 ext = os.path.splitext(file)[-1].lower()
                 if ext == ".csv":
                     pass
-=======
             file_dir = "...."
             clean_input_data(file_dir, 'wea')
->>>>>>> e6dc182c2c328896ca1966757eb86587862523ad
-        
         return
 
-    def 
+    def test_drop_dup_nun_smoke(self):
+        """
+        Smoke test to check the function runs.
+        """
+        wea_df = read_wea_datas(
+            data_dir, 'wea')  # Q: WILL IT KNOW READ_WEA_DATAS?
+        drop_dup_nan(wea_df, 'date')  # Q: WILL IT KNOW DROP_DUP_NAN?
+        return
+
+    def test_merge_df_edge1(self):
+        """
+        Check there are multiple dataframes.
+        """

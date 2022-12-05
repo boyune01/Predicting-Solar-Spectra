9f14816c3b702850a3e19defa3c2633f0c24eeaf
TylerShakibai <tyler.shakibai@gmail.com>
2022-12-05 00:17:10 -0800

    Split ml development scripts

---
+++ code/neural_network/sk_mlp.py
@@ -0,0 +1,18 @@
+from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+X = np.loadtxt('data\input_cleaned\wea_input.csv', skiprows=1, delimiter=',', usecols=range(1, 14))
+y = np.loadtxt(r'data\input_cleaned\rad_input.csv', skiprows=1, delimiter=',', usecols=range(1, 14))
+
+for i in [X, y]:
+    print(type(i), i.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=1)
+
+reg = MLPRegressor(hidden_layer_sizes=(64, 64), activation='relu', random_state=1, max_iter=1000).fit(X_train, y_train)
+pred = reg.predict(X_test)
+print(pred)
+print(pred.shape)
+acc = reg.score(X_test, y_test)
+print(acc)

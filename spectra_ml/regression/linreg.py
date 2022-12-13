1632698ee72f492acff7632cd0919ed041c5a2d2
themacfreezie <bmcgreal@uw.edu>
2022-12-13 12:39:48 -0800

    Renamed *_func files in regression folder. All regression code function wrapped

--- spectra_ml/regression/linreg.py
+++ spectra_ml/regression/linreg.py
@@ -9,31 +9,39 @@ import statsmodels.api as sm
 
 pd.set_option('display.max_columns', None)
 
+
+# define linreg function
+def linreg(df):
+
+    print(df)
+
+    X = df[['Zenith Angle [degrees]', 'Azimuth Angle [degrees]',
+            'Total Cloud Cover [%]', 'Opaque Cloud Cover [%]',
+            'AOD [400nm]', 'AOD [500nm]', 'AOD [675nm]',
+            'AOD [870nm]', 'AOD [1020nm]', 'SSA [675nm]',
+            'Asymmetry [675nm]',
+            'Precipitable Water [mm]']]
+    y = df['cct']
+
+    """
+    statsmodels approach
+    returns table of results including params and significance
+    as well as general evaluations of regression fitness
+    prints results and creates 'results' as dataframe
+    """
+
+    X2 = sm.add_constant(X)
+    est = sm.OLS(y, X2)
+    est2 = est.fit()
+    print(est2.summary())
+
+    print('')
+    results = (est2.summary().tables[1])
+    print(results)
+
+
 # reading in data and defining X matrix & y vector
 df = pd.read_csv('../../data/input_cleaned/linreg.csv')
-print(df)
 
-X = df[['Zenith Angle [degrees]', 'Azimuth Angle [degrees]',
-        'Total Cloud Cover [%]', 'Opaque Cloud Cover [%]',
-        'AOD [400nm]', 'AOD [500nm]', 'AOD [675nm]',
-        'AOD [870nm]', 'AOD [1020nm]', 'SSA [675nm]',
-        'Asymmetry [675nm]',
-        'Precipitable Water [mm]']]
-y = df['cct']
-
-
-"""
-statsmodels approach
-returns table of results including params and significance
-as well as general evaluations of regression fitness
-prints results and creates 'results' as dataframe
-"""
-
-X2 = sm.add_constant(X)
-est = sm.OLS(y, X2)
-est2 = est.fit()
-print(est2.summary())
-
-print('')
-results = (est2.summary().tables[1])
-print(results)
+# run linreg function
+linreg(df)
